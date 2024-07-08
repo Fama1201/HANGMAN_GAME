b8a01e3709a55d2c3e062fec3b7f7dbce4d0949b
Francisco Molina <francisco.molina.antonio@gmail.com>
2024-07-08 01:27:48 +0100

    BANK_WORLD ADDED

--- GAME.C
+++ GAME.C
@@ -1,5 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> // to sleep
+#include <ctime>
+#define NUM_WORDS 100
+
+
+const char *words[NUM_WORDS]={
+    "apple", "banana", "cherry", "date", "elderberry",
+    "fig", "grape", "honeydew", "kiwi", "lemon",
+    "mango", "nectarine", "orange", "papaya", "quince",
+    "raspberry", "strawberry", "tangerine", "ugli", "vanilla",
+    "watermelon", "xigua", "yellowfruit", "zucchini", "apricot",
+    "blackberry", "blueberry", "cantaloupe", "dragonfruit", "gooseberry",
+    "guava", "jackfruit", "kumquat", "lime", "lychee",
+    "mandarin", "mulberry", "olive", "passionfruit", "peach",
+    "pear", "persimmon", "pineapple", "plum", "pomegranate",
+    "prune", "pumpkin", "rhubarb", "starfruit", "tomato",
+    "cucumber", "avocado", "artichoke", "broccoli", "carrot",
+    "cauliflower", "celery", "chard", "chicory", "collard",
+    "corn", "eggplant", "fennel", "kale", "lettuce",
+    "mushroom", "onion", "pepper", "potato", "radish",
+    "spinach", "squash", "turnip", "yam", "zucchini",
+    "asparagus", "beet", "bellpepper", "bokchoy", "brusselsprout",
+    "cabbage", "cassava", "chayote", "cilantro", "dandelion",
+    "endive", "garlic", "ginger", "horseradish", "jicama",
+    "kohlrabi", "leek", "okra", "parsley", "parsnip",
+    "rutabaga", "scallion", "shallot", "sorrel", "taro"
+
+};
+
+
 
 void clearScreen() {
     // Clear the screen 
@@ -8,6 +38,8 @@ void clearScreen() {
     #else
         system("clear");
     #endif 
+
+
 }
 //Tittle of the game
 void printTitle() {
@@ -29,7 +61,7 @@ void printTitle() {
     printf("************************************************************************************************************\n");
 }
 
-//Menu to select options
+//Menu to select
 void printMenu() {
     printf("\n1. Play\n");
     printf("2. Instructions\n");
@@ -37,8 +69,17 @@ void printMenu() {
     printf("Select an option: ");
 }
 
+const char* get_random_word() {
+                int random_index = rand() % NUM_WORDS;
+                return words[random_index];
+                } 
+
 int main() {
     int option;
+    char palabra[100];
+    const char *random_word;
+
+    srand(time(0)); // Inicializa el generador de números aleatorios con la hora actual
 
     do {
         clearScreen();
@@ -48,22 +89,32 @@ int main() {
 
         switch (option) {
             case 1:
-                // Here goes the game logic
+                // GAME
                 printf("Starting game...\n");
+                sleep(1); // Pausa 1 segundo
+                clearScreen();
+
+                random_word = get_random_word(); // Obtiene una palabra al azar
+                printf("Random word: %s\n", random_word); // Imprime la palabra seleccionada
+
+                // Aquí implementa la lógica del juego de ahorcado
+
                 break;
             case 2:
-                // Here the instructions will be displayed
+                // Instrucciones del juego
                 printf("Instructions: Guess the word before the hangman is complete.\n");
                 break;
             case 3:
+                // Salida del programa
                 printf("Exiting...\n");
                 break;
             default:
                 printf("Invalid option. Please try again.\n");
         }
+
         printf("Press enter to continue...");
-        getchar(); // Capture enter after selecting the option
-        getchar(); // Pause execution until user presses enter
+        getchar(); // Captura el enter después de seleccionar la opción
+        getchar(); // Pausa la ejecución hasta que el usuario presione enter
     } while (option != 3);
 
     return 0;
